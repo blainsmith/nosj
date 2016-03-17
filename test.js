c7d1ff90174263c7985f38837f17b98104294524
Blain Smith <rebelgeek@blainsmith.com>
2016-03-17 08:59:59 -0400

    force UTC for tests and travis

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+process.env.TZ = 'UTC';
+
 import test from 'ava';
 import nosj from './';
 
@@ -5,7 +7,7 @@ const object = {
 	string: 'hello',
 	number: 13,
 	float: 3.14,
-	date: new Date('Wed Jan 13 1982 00:00:00 GMT-0500 (EST)'),
+	date: new Date('Wed Jan 13 1982 00:00:00 GMT+0000 (UTC)'),
 	array: [
 		{a: 'a'},
 		{b: 'b'}
@@ -20,7 +22,7 @@ const objectStringDates = {
 	string: 'hello',
 	number: 13,
 	float: 3.14,
-	date: 'Wed Jan 13 1982 00:00:00 GMT-0500 (EST)',
+	date: 'Wed Jan 13 1982 00:00:00 GMT+0000 (UTC)',
 	array: [
 		{a: 'a'},
 		{b: 'b'}
@@ -31,7 +33,7 @@ const objectStringDates = {
 	}
 };
 
-const string = '{"string":"hello","number":13,"float":3.14,"date":"Wed Jan 13 1982 00:00:00 GMT-0500 (EST)","array":[{"a":"a"},{"b":"b"}],"object":{"x":"x","y":"y"}}';
+const string = '{"string":"hello","number":13,"float":3.14,"date":"Wed Jan 13 1982 00:00:00 GMT+0000 (UTC)","array":[{"a":"a"},{"b":"b"}],"object":{"x":"x","y":"y"}}';
 
 test('serialize', t => {
 	t.is(nosj.serialize(object), string);
