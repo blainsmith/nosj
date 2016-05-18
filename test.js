754603e764141c6a77f400e7fe3c17647ea9c682
Blain Smith <rebelgeek@blainsmith.com>
2016-05-18 08:37:16 -0400

    accept ISO 8601 dates for consistancy

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ import nosj from './';
 
 const object = {
 	string: 'hello',
+	stringNumber: '123',
 	number: 13,
 	float: 3.14,
 	date: new Date('Wed Jan 13 1982 00:00:00 GMT+0000 (UTC)'),
@@ -20,9 +21,10 @@ const object = {
 
 const objectStringDates = {
 	string: 'hello',
+	stringNumber: '123',
 	number: 13,
 	float: 3.14,
-	date: 'Wed Jan 13 1982 00:00:00 GMT+0000 (UTC)',
+	date: '1982-01-13T00:00:00.000Z',
 	array: [
 		{a: 'a'},
 		{b: 'b'}
@@ -33,7 +35,7 @@ const objectStringDates = {
 	}
 };
 
-const string = '{"string":"hello","number":13,"float":3.14,"date":"Wed Jan 13 1982 00:00:00 GMT+0000 (UTC)","array":[{"a":"a"},{"b":"b"}],"object":{"x":"x","y":"y"}}';
+const string = '{"string":"hello","stringNumber":"123","number":13,"float":3.14,"date":"1982-01-13T00:00:00.000Z","array":[{"a":"a"},{"b":"b"}],"object":{"x":"x","y":"y"}}';
 
 test('serialize', t => {
 	t.is(nosj.serialize(object), string);
